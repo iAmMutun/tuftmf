3858b13caf187405c28d8a33d39c4be75a0cfd42
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2014-09-02 21:04:55 +0700

    Handles non-existing directory more properly

--- scan_dir.cpp
+++ scan_dir.cpp
@@ -7,6 +7,7 @@ void fix_file(const std::wstring& in, const std::wstring& out);
 
 void scan_dir(const std::wstring& a, const std::wstring& b)
 {
+  CreateDirectoryW(b.c_str(), NULL);
   std::wstring in = a;
   std::wstring out = b;
   if (in.back() != L'\\')
@@ -26,9 +27,7 @@ void scan_dir(const std::wstring& a, const std::wstring& b)
     if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
       std::wstring dir = ffd.cFileName;
       if (dir != L"." && dir != L"..") {
-        std::wstring out_dir = out + dir;
-        CreateDirectoryW(out_dir.c_str(), NULL);
-        scan_dir(in + dir, out_dir);
+        scan_dir(in + dir, out + dir);
       }
     } else {
       std::wstring file = ffd.cFileName;
