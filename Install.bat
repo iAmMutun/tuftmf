478e5652e25304a618d559a6cabaa6e0bf0fe325
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2017-02-18 01:46:25 +0700

    Update script.

--- Install.bat
+++ Install.bat
@@ -1,9 +1,5 @@
 @ECHO off
-SET "dir=%homedrive%%homepath%\Documents\BanishedMod\BanishedTranslationTH"
+SET "dir=..\BanishedTranslationTH"
 @ECHO on
 COPY "Release\BanishedTHUnicodeFix.exe" "%dir%\UnicodeFix32.exe"
 COPY "x64\Release\BanishedTHUnicodeFix.exe" "%dir%\UnicodeFix64.exe"
-
-@ECHO off
-:: delay hack for win xp
-ping 127.0.0.1 -n 2 > NUL
