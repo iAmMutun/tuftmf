69cb31ab0a08222603e61ed75c5b8752a0d61214
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2018-03-20 21:12:37 +0700

    Remove Install.bat

--- Install.bat
+++
@@ -1,6 +0,0 @@
-@ECHO off
-SET "bin=\Release\BanishedTHUnicodeFix.exe"
-SET "dir=..\BanishedTranslationTH\"
-@ECHO on
-COPY "Win32%bin%" "%dir%UnicodeFix32.exe"
-COPY "x64%bin%" "%dir%UnicodeFix64.exe"
