86023202858c322a012ff21c4ffb67966e639915
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2018-03-21 23:48:35 +0700

    Wrap chars constants under `tuftmf` namespace.

--- chars.h
+++ chars.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <cstdint>
 
-static const uint16_t _double_quote = L'\"';
+namespace tuftmf
+{
 
-static const uint16_t _at_sign = L'@';
+const uint16_t _double_quote = L'\"';
 
-static const uint16_t _zero_width_space = L'\x200B';
+const uint16_t _at_sign = L'@';
 
-static const uint16_t _digits[] =
+const uint16_t _zero_width_space = L'\x200B';
+
+const uint16_t _digits[] =
 {
     L'0', L'1', L'2', L'3', L'4', L'5', L'6', L'7', L'8', L'9'
 };
 
-static const uint16_t _floating_vowels[] =
+const uint16_t _floating_vowels[] =
 {
     L'\x0E31', // ั
     L'\x0E34', // ิ
@@ -22,7 +25,7 @@ static const uint16_t _floating_vowels[] =
     L'\x0E47', // ็
 };
 
-static const uint16_t _floating_vowels_left[] =
+const uint16_t _floating_vowels_left[] =
 {
     L'\xF710', // ั
     L'\xF701', // ิ
@@ -32,19 +35,19 @@ static const uint16_t _floating_vowels_left[] =
     L'\xF712', // ็
 };
 
-static const uint16_t _backward_floating_vowels[] =
+const uint16_t _backward_floating_vowels[] =
 {
     L'\x0E33', // ำ
 };
 
-static const uint16_t _sub_vowels[] =
+const uint16_t _sub_vowels[] =
 {
     L'\x0E38', // ุ
     L'\x0E39', // ู
     L'\x0E3A', // ฺ
 };
 
-static const uint16_t _long_tail[] =
+const uint16_t _long_tail[] =
 {
     L'\x0E1B', // ป
     L'\x0E1D', // ฝ
@@ -52,7 +55,7 @@ static const uint16_t _long_tail[] =
     L'\x0E2C', // ฬ
 };
 
-static const uint16_t _tone_marker_high[] =
+const uint16_t _tone_marker_high[] =
 {
     L'\x0E48', // '
     L'\x0E49', // ้
@@ -61,7 +64,7 @@ static const uint16_t _tone_marker_high[] =
     L'\x0E4C', // ์
 };
 
-static const uint16_t _tone_marker_left_low[] =
+const uint16_t _tone_marker_left_low[] =
 {
     L'\xF705', // '
     L'\xF706', // ้
@@ -70,7 +73,7 @@ static const uint16_t _tone_marker_left_low[] =
     L'\xF709', // ์
 };
 
-static const uint16_t _tone_marker_low[] =
+const uint16_t _tone_marker_low[] =
 {
     L'\xF70A', // '
     L'\xF70B', // ้
@@ -79,7 +82,7 @@ static const uint16_t _tone_marker_low[] =
     L'\xF70E', // ์
 };
 
-static const uint16_t _tone_marker_left_high[] =
+const uint16_t _tone_marker_left_high[] =
 {
     L'\xF713', // '
     L'\xF714', // ้
@@ -88,36 +91,38 @@ static const uint16_t _tone_marker_left_high[] =
     L'\xF717', // ์
 };
 
-static const uint16_t _lower_foot[] =
+const uint16_t _lower_foot[] =
 {
     L'\x0E0D', // ญ
     L'\x0E10', // ฐ
 };
 
-static const uint16_t _lower_foot_stripped[] =
+const uint16_t _lower_foot_stripped[] =
 {
     L'\xF70F', // ญ
     L'\xF700', // ฐ
 };
 
-static const uint16_t _lower_vowels[] =
+const uint16_t _lower_vowels[] =
 {
     L'\x0E38', // ุ
     L'\x0E39', // ู
     L'\x0E3A', // ฺ
 };
 
-static const uint16_t _lower_vowels_left[] =
+const uint16_t _lower_vowels_left[] =
 {
     L'\xF718', // ุ
     L'\xF719', // ู
     L'\xF71A', // ฺ
 };
 
-static const uint16_t _lower_tail[] =
+const uint16_t _lower_tail[] =
 {
     L'\x0E0E', // ฎ
     L'\x0E0F', // ฏ
     L'\x0E24', // ฤ
     L'\x0E26', // ฦ
 };
+
+} // namespace tuftmf
