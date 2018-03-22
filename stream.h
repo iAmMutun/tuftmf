ce60b2e6e52b6d9d995d07a83c2a766ccb24d55f
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2018-03-22 21:05:03 +0700

    Rename `_Ttraits` to `char_traits`. Move `_Tcvtutf8` into `fix()` and rename to `cvtutf8`.

--- stream.h
+++ stream.h
@@ -10,23 +10,24 @@
 namespace tuftmf
 {
 
-using _Ttraits  = std::char_traits<char_type>;
-using _Tcvtutf8 = std::codecvt_utf8<char_type, 0x10ffff, std::consume_header>;
+using char_traits  = std::char_traits<char_type>;
 
-using istream = std::basic_istream<char_type, _Ttraits>;
-using ostream = std::basic_ostream<char_type, _Ttraits>;
+using istream = std::basic_istream<char_type, char_traits>;
+using ostream = std::basic_ostream<char_type, char_traits>;
 
-using ifstream = std::basic_ifstream<char_type, _Ttraits>;
-using ofstream = std::basic_ofstream<char_type, _Ttraits>;
+using ifstream = std::basic_ifstream<char_type, char_traits>;
+using ofstream = std::basic_ofstream<char_type, char_traits>;
 
-using istringstream = std::basic_istringstream<char_type, _Ttraits>;
-using ostringstream = std::basic_ostringstream<char_type, _Ttraits>;
+using istringstream = std::basic_istringstream<char_type, char_traits>;
+using ostringstream = std::basic_ostringstream<char_type, char_traits>;
 
 template<class _IStream, class _OStream>
 void fix(_IStream& is, _OStream& os)
 {
-    is.imbue(std::locale(is.getloc(), new _Tcvtutf8));
-    os.imbue(std::locale(os.getloc(), new _Tcvtutf8));
+    using cvtutf8 = std::codecvt_utf8
+        <char_type, 0x10ffff, std::consume_header>;
+    is.imbue(std::locale(is.getloc(), new cvtutf8));
+    os.imbue(std::locale(os.getloc(), new cvtutf8));
     char_type before = L'\0', current, after;
     is.get(current);
     while (!is.eof())
